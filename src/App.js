cbff33ada7f28ebc7dec269ca811a6a0b64e78ce
TM_Brodie <brodie.balser@ticketmaster.com>
2023-09-16 15:00:28 -0600

    created a home page with logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import MoneyManager from './pages/MoneyManager'
+import Home from './pages/Home'
 import { initApp } from './state/actions';
 
 function App({initApp}) {
@@ -9,6 +10,7 @@ function App({initApp}) {
 
   return (
     <div className="App">
+      <Home/>
       <MoneyManager/>
     </div>
   );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './index.css';
+import { Heading, HeadingLevel } from 'baseui/heading';
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className='Logo'></div>
+    </div>
+  );
+}
+
+export default Home
