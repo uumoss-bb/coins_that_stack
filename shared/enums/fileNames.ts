ae271a810c16b00a8c7a0fdb6f96fd6d3088a348
TM_Brodie <brodie.balser@ticketmaster.com>
2024-10-08 11:43:14 -0400

    Brodie is commiting something on git

--- shared/enums/fileNames.ts
+++ shared/enums/fileNames.ts
@@ -5,6 +5,6 @@ export const STACK_FILE_NAME = 'stacks'
 
 export const TRANSACTIONS_FILE_NAME = 'transactions'
 
-export const DIRTY_TRANSACTIONS= './_dirtyTransactions'
+export const DIRTY_TRANSACTIONS= './.dirtyTransactions'
 
-export const PERSONAL_STORAGE = './_personalStorage'
+export const PERSONAL_STORAGE = './.personalStorage'
